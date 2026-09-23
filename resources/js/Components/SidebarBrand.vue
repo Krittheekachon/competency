e9028ed82d356efedb5c3ed9451a3fc527678483
Krittheekachon <krittkeekachon.s@kkumail.com>
2026-09-24 02:18:24 +0700

    เปลี่ยนหนา้ login logo เป็น en-idp

--- resources/js/Components/SidebarBrand.vue
+++ resources/js/Components/SidebarBrand.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="sb-logo">
-        <img
+        <svg
             class="sb-brand-image"
-            src="/images/CIDP_encompetency-nobg.png"
-            alt="CIDP Competency and IDP System"
-        />
-        <div class="sb-brand-title">
-            ระบบบริหารสมรรถนะและ<br />
-            แผนพัฒนารายบุคคล
-        </div>
+            viewBox="40 58 470 210"
+            role="img"
+            aria-label="EN-IDP"
+        >
+            <g fill="#8f3036">
+                <path d="M185 65 47 127v135h153v-29H79v-24l106-47v-31L79 178v-29l106-47z" />
+                <path d="M205 65h23l105 122V109l28-13v166h-30L233 149v113h-28z" />
+            </g>
+            <g fill="none" stroke="#8f3036" stroke-width="4" stroke-linejoin="miter" stroke-linecap="square">
+                <path d="M375 261V119l53-25v168m-38-8V130l22-10v135m16-95 22 10v92m-22-50 72 41v9" />
+            </g>
+        </svg>
     </div>
 </template>
